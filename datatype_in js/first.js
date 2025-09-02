226af8bca4d57e7e3172e468e12f060505462d36
Papri Bhattacharjee <bhpapri03@gmail.com>
2025-09-02 12:16:34 +0530

    Add files via upload

---
+++ datatype_in js/first.js
@@ -0,0 +1,23 @@
+console.log("Papri Bhattacharjee");
+var r=5;
+var area=3.14*r*r;
+let name="Papri";  //string 
+console.log(area);
+console.log(name);
+console.log(typeof area,typeof name);
+console.log(area+1); 
+num=14;  /**var na likhleo hoi but var lekha ii better or let o lekha jai or const*/
+console.log(num);
+let isFollow=false ; //boolean
+console.log(typeof isFollow);  
+
+
+//NON primitive datatype
+let student={
+    name: "Papri",
+    age:21,
+    class:12,
+};
+console.log(student);
+student.name="Niladri";
+console.log(student);
---
+++ datatype_in js/prac.js
@@ -0,0 +1,8 @@
+let product={
+    name:"Parker Jotter Standard CT Ball Pen",
+    type:"(Black)",
+    rating:7002,
+    MRP:285,
+    discount:5,
+};
+console.log(product);
