1cb4c054cc9f73be67336f0fdd06e4a64a1bb137
papri221003 <bhpapri03@gmail.com>
2025-09-04 18:44:29 +0530

    modified

--- callback_promises_async/app.js
+++ callback_promises_async/app.js
@@ -28,6 +28,8 @@ calculator(1,2,(a,b)=>{
 })
 */
 
+//callback hell understanding
+/*
 function getData(dataId,getNextData)
 {
     setTimeout(()=>{
@@ -47,4 +49,71 @@ getData(1,()=>{
             })
         });
     });
+})
+
+*/
+
+
+
+//Promise
+/*
+let getPromise = ()=>{
+return new Promise((resolve,reject)=>{
+    console.log("I am a promise");
+    reject("there is some error");
+})
+};
+
+let promise=getPromise();
+promise.then((res)=>
+{
+    console.log("promise fulfilled",res);
+});
+promise.catch((err)=>{
+    console.log("rejected",err);
+});
+*/
+
+
+/*function getData(dataId,getNextData)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log("Data: ",dataId);
+        resolve("Success");
+        if(getNextData)
+        {
+            getNextData();
+        }
+    },5000);
+    }) ;
+}*/
+//Promise chain
+function async()
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data1");
+            resolve("Succuess");
+        },4000);
+    })
+}
+function async2()
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("data2");
+            resolve("Succuess again");
+        },4000);
+    })
+}
+console.log("Fetching data1............");
+let p1=async();
+p1.then((res)=>{
+   // console.log(res);
+    console.log("Fetching data2............");
+    let p2=async2();
+    p2.then((res)=>{
+        //console.log(res);
+    })
 })
