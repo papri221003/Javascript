7727aca9781f215ce0e9515f1410196ab93d3047
papri221003 <bhpapri03@gmail.com>
2025-09-18 09:21:44 +0530

    initial commit

---
+++ Conversion_Operations/ConOperation.js
@@ -0,0 +1,16 @@
+let score = "Papri"
+
+console.log(typeof score);
+
+//Typecasting
+
+let typecastVal= Number(score);     //typecasting
+console.log(typecastVal);    //as typecasted no a strin it cant be a number so it shows NaN(Not a Number)
+console.log(typeof typecastVal);   
+
+let b= null;
+console.log(typeof b);
+
+let convertedB=Number(b);
+console.log(typeof convertedB);
+console.log(convertedB);
