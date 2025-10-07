476b6ab079ae8e6eeb4a45537c2b60f9d1e29b38
papri221003 <bhpapri03@gmail.com>
2025-10-07 13:41:16 +0530

    initial commit

---
+++ objects_js/script.js
@@ -0,0 +1,31 @@
+
+const JSuser={
+    name: "Papri",
+    age: 22,
+    location: "Konnagar",
+    Email: "pb@gmail.com",
+}
+
+console.log(JSuser);
+
+
+JSuser.Email="jhv@gmail.com";
+console.log(JSuser.Email);
+
+console.log(JSuser["Email"]);
+
+
+
+//Adding Function into the object using arrow function
+JSuser.greet2=function(){
+    console.log(`Hello!! welcome to the JS world,${this.name}`);
+}
+
+
+//Adding Function into the object using arrow function
+JSuser.greet=()=>{
+    console.log(`Hello!! welcome to the JS world,${JSuser.name}`);
+}
+
+JSuser.greet();
+JSuser.greet2();
