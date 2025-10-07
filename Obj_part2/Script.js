92d6977ccba4e10da2f2c54fa20de46401fa038c
papri221003 <bhpapri03@gmail.com>
2025-10-07 15:51:54 +0530

    initial commit

--- Obj_part2/Script.js
+++ Obj_part2/Script.js
@@ -16,3 +16,16 @@ console.log(singleTone_obj.username.fullname);
 console.log(Object.keys(singleTone_obj));  //it shows what are the keys in my object
 console.log(Object.values(singleTone_obj));   //it shows what are the values in my object
 console.log(Object.entries(singleTone_obj));   //it shows both key and value pair in 1st object not the nested one(It just shows the key and respective values acc to our given input as per our entry)
+
+
+
+//Part 2
+//Object Destructure
+const course={
+    coursename: "JS lecture",
+    courseInstructor: "Hitesh",
+    coursePrice: 999,
+}
+
+const {courseInstructor:ins}=course;
+console.log(ins);
