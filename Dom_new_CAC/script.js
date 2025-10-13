073badbc730378af99a0e869f02d6a5ebc756382
papri221003 <bhpapri03@gmail.com>
2025-10-13 11:02:57 +0530

    initial commit

--- Dom_new_CAC/script.js
+++ Dom_new_CAC/script.js
@@ -1,2 +1,40 @@
-let a=document.getElementById("Myheading");
-console.dir(a);
+let para=document.getElementById('new_para');
+para.style.backgroundColor='green';
+
+console.log(para.textContent)
+console.log(para.innerHTML)
+console.log(para.innerText)
+let tag=para.tagName;
+console.log(tag);
+
+
+let body=document.querySelector('body');
+//console.log(body);
+
+body.style.backgroundColor='white';
+body.style.color='black';
+
+console.dir(document.querySelectorAll('h1'));
+console.dir(document.querySelector('h1'));
+
+
+//document.querySelectorAll('li');
+let mylist=document.querySelectorAll('li');
+/*
+mylist[0].style.backgroundColor="blue";
+mylist[2].style.backgroundColor="purple";
+mylist[1].style.backgroundColor="violet";
+mylist[3].style.backgroundColor="pink";
+*/
+
+mylist.forEach((val)=>{
+   // val.style.backgroundColor="pink";
+})
+
+let newlist=document.getElementsByClassName("list_item");//gives HTML Collection
+let newlistArray=Array.from(newlist);//convert HTML collection into the array
+newlistArray.forEach((li)=>{
+    li.style.backgroundColor="violet";
+})
+
+
